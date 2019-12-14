cec8267e1a1041d69bf9bc9064b8d9ef7d546437
Marc Eder <6818607+meder411@users.noreply.github.com>
2019-12-14 18:08:05 -0500

    Bump version: 0.0.1 → 0.0.2

--- package/setup.py
+++ package/setup.py
@@ -56,7 +56,7 @@ def extension(name,
 
 setup(
     name='Tangent Images',
-    version='0.0.1',
+    version='0.0.2',
     author='Marc Eder',
     author_email='meder@cs.unc.edu',
     description='A PyTorch module for tangent_images',
